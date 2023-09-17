dc5dbcba9320337c41532d31ddc45328835ba71f
Pavlo <pashaplaylopatuk@gmail.com>
2023-09-17 18:07:46 +0300

    update

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,8 +1,17 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import useDataStore from './store/dataStore'
 import { movieDetailsType } from './types'
+import { videosResponseType } from './types'
+import loadVideos from './services/loadVideos'
+import { Video } from './types'
+import Link from 'next/link'
+
+async function load(id: string, func: any) {
+	const res = await loadVideos(id)
+	func(res?.results)
+}
 
 const page = ({ params }: { params: { id: string } }) => {
 
@@ -14,6 +23,15 @@ const page = ({ params }: { params: { id: string } }) => {
 		loadData(params.id)
 	}, [])
 
+	const [videos, setVideos] = useState<Video[]>()
+	useEffect(() => {
+		load(params.id, setVideos)
+	}, [])
+
+	useEffect(() => {
+		console.log(videos)
+	}, [videos])
+
 	return (
 		<div>
 			{
@@ -21,34 +39,69 @@ const page = ({ params }: { params: { id: string } }) => {
 					<div className="">Loading...</div>
 					:
 
-					<div className="space-y-3">
-						<img src={'https://image.tmdb.org/t/p/w500' + movieData.poster_path} alt=""
-							className='w-30'
-						/>
-						
-						<div className="px-2">
-							<div className="">
+					<div className="space-y-4">
+
+
+						<div className="">
+							<div className="font-bold text-3xl ml-2">
 								{
 									movieData?.title
 								}
-							<div className="">
-								{
-									movieData?.release_date.slice(0, 4)
-								}
 							</div>
-							<ul className="">
-								{
-									movieData.genres.map( (e) => (
-										<li className="">{e.name}</li>
-									))
-								}
-							</ul>
-							<div className="">
-								{
-									movieData.overview
-								}
+							<div className="ml-1 flex space-x-3">
+								<div className="">
+									{
+										movieData?.release_date.slice(0, 4)
+									}
+								</div>
+								<div className="">
+									{
+										movieData?.runtime
+									}
+									<span className="text-neutral-300"> min</span>
+								</div>
 							</div>
+						</div>
+
+						<iframe
+							src={`https://www.youtube.com/embed/${videos?.at(0).key}`}
+							className='w-[100vw]'
+						// width={videos?.at(0)?.size}
+						></iframe>
+
+						<div className="grid grid-cols-2">
+							<Link className="hover:bg-neutral-800 h-10 flex items-center justify-center w-full " href="">Videos</Link>
+							<Link className="hover:bg-neutral-800 h-10 flex items-center justify-center w-full " href="">Images</Link>
+						</div>
+
+						<div className="flex px-2 h-60">
+
+							<img src={'https://image.tmdb.org/t/p/w500' + movieData.poster_path}
+								alt="movie poster"
+								className='w-35 object-contain'
+							/>
+
+							<div className="px-1">
+								<ul className="flex space-x-2">
+									{
+										movieData
+										.genres
+										.slice(0, 3)
+										.map((e) => (
+											<li 
+											className="px-2 py-1 rounded-2xl border border-neutral-500 hover:bg-neutral-800 text-xs"
+											key={e.id}
+											>{e.name.split(' ').at(0)}</li>
+										))
+									}
+								</ul>
+								<div className="py-2 px-4 text-xs text-neutral-200">
+									{
+										movieData.overview
+									}
+								</div>
 							</div>
+
 						</div>
 					</div>
 
--- app/movie/[id]/types.ts
+++ app/movie/[id]/types.ts
@@ -1,31 +1,48 @@
 export type movieDetailsType = {
-        adult: boolean;
-        backdrop_path: string | null;
-        belongs_to_collection: any | null;
-        budget: number;
-        genres: any[]; // You can replace 'any' with a more specific type if you have genre data structure.
-        homepage: string;
-        id: number;
-        imdb_id: string;
-        original_language: string;
-        original_title: string;
-        overview: string;
-        popularity: number;
-        poster_path: string | null;
-        production_companies: any[]; // You can replace 'any' with a more specific type if you have production company data structure.
-        production_countries: {
-          iso_3166_1: string;
-          name: string;
-        }[];
-        release_date: string;
-        revenue: number;
-        runtime: number;
-        spoken_languages: any[]; // You can replace 'any' with a more specific type if you have spoken language data structure.
-        status: string;
-        tagline: string;
-        title: string;
-        video: boolean;
-        vote_average: number;
-        vote_count: number;
-      };
-      
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: any | null;
+  budget: number;
+  genres: any[]; // You can replace 'any' with a more specific type if you have genre data structure.
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: any[]; // You can replace 'any' with a more specific type if you have production company data structure.
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: any[]; // You can replace 'any' with a more specific type if you have spoken language data structure.
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type Video = {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+export type videosResponseType = {
+  id: number
+  results: Video[]
+}
